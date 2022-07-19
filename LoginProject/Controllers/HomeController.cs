990cd68818588f4fb65b4353692ac670d763e60b
sneha gayathri <snehagayathri1418@gmail.com>
2022-07-19 15:50:14 +0530

    Add project files.

---
+++ LoginProject/Controllers/HomeController.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using LoginProject.Models;
+
+namespace LoginProject.Controllers
+{
+    public class HomeController : Controller
+    {
+        gayathriEntities db = new gayathriEntities();
+        // GET: Home
+
+        public ActionResult Index()
+        {
+            return View(db.userinfoes.ToList());
+        }
+        public ActionResult Signup()
+        {
+            return View();
+       }
+        [HttpPost]
+        public ActionResult Signup(userinfo userinfo)
+        {
+                if (db.userinfoes.Any(x => x.username == userinfo.username))
+                {
+                    ViewBag.Notification = "This account is already existed";
+                return View();
+
+            }
+           
+            else
+            {
+                db.userinfoes.Add(userinfo);
+                db.SaveChanges();
+
+
+                Session["idSS"] = userinfo.id.ToString();
+                Session["usernameSS"] = userinfo.username.ToString();
+                return RedirectToAction("Index", "Home");
+
+            }
+        }
+        public ActionResult Logout()
+        {
+            Session.Clear();
+            return RedirectToAction("Index", "Home");
+        }
+        [HttpGet]
+        public ActionResult Login()
+        {
+            return View();
+        }
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Login(userinfo userinfo)
+        {
+            var checkLogin = db.userinfoes.Where(x => x.username.Equals(userinfo.username) && x.password.Equals(userinfo.password)).FirstOrDefault();
+            if(checkLogin !=null)
+            {
+                Session["idSS"] = userinfo.id.ToString();
+                Session["usernameSS"] = userinfo.username.ToString();
+                return RedirectToAction("Index", "Home");
+            }
+            else
+            {
+                ViewBag.Notification = "wrong username or password";
+            }
+            return View();
+        }
+        }
+}
---
+++ LoginProject/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="LoginProject.MvcApplication" Language="C#" %>
